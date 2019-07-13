32d7c9fd2aa1a063b4dc61f05af86ef5888602db
Cristian-Manriquez <52456482+Cristian-Manriquez@users.noreply.github.com>
2019-07-12 21:08:50 -0400

    Add files via upload

---
+++ Problemas(1-10)Basicos.py
@@ -0,0 +1,21 @@
+#Problema 2
+print("Cristian\nHarold\nManriquez\nHelguero")
+#Problema 3
+print("Mecatronica"*5)
+print("Ingenieria"*3)
+#Problema4
+print("Este","es","un","codigo","en","Python",sep="***")
+#Problema 5
+print("Monty Python",end="*")
+print("es muy gracioso",end="**")
+print("¿Lo sabias?")
+#Problema 6
+print("MeCAtronICA".lower())
+#Problema 7
+print("mecatronica".upper())
+#Problema 8
+print("mecatronica".capitalize())
+#Problema 9
+print("nosotros estudiamos meacatronica".title())
+#Problema 10
+print(("Monty Python es un personaje de comedia".split()))
